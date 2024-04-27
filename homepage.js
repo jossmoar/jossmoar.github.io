eb0077292b662cd4e8196336f9c6f55f0583919b
Joselin <jmonteroa1@ucenfotec.ac.cr>
2024-04-27 11:16:03 -0600

    Primer commit

---
+++ homepage.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.carousel').carousel();
+  });
+  
+     
+
+      function redirigirAIndex() {
+        window.location.href = 'index.html';
+      }
+
+
+      window.onscroll = function() {scrollFunction()};
+
+      function scrollFunction() {
+          if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+              document.getElementById("scrollTopButton").style.display = "block"; /* Mostrar el botón cuando se desplaza hacia abajo */
+          } else {
+              document.getElementById("scrollTopButton").style.display = "none"; /* Ocultar el botón cuando está en la parte superior */
+          }
+      }
+      
+      function scrollToTop() {
+          document.body.scrollTop = 0; /* Para Safari */
+          document.documentElement.scrollTop = 0; /* Para otros navegadores */
+      }
+      
